182957e2be32abf65537668463a67adf18ea4ac3
Paul <Paul@.(none)>
2013-07-18 18:06:54 -0300

    add key for blockchain storage

---
+++ BitSharp.Data/BlockchainKey.cs
@@ -0,0 +1,25 @@
+﻿using BitSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSharp.Data
+{
+    public struct BlockchainKey
+    {
+        private readonly Guid _guid;
+        private readonly UInt256 _rootBlockHash;
+
+        public BlockchainKey(Guid guid, UInt256 rootBlockHash)
+        {
+            this._guid = guid;
+            this._rootBlockHash = rootBlockHash;
+        }
+
+        public Guid Guid { get { return this._guid; } }
+
+        public UInt256 RootBlockHash { get { return this._rootBlockHash; } }
+    }
+}
