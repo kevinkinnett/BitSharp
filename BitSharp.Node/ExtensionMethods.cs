f18163df03a51d28812075eb69244e96005933c2
Paul <Paul@.(none)>
2013-07-18 18:07:10 -0300

    move network address extension methods into node project

---
+++ BitSharp.Node/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using BitSharp.WireProtocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSharp.Node.ExtensionMethods
+{
+    public static class ExtensionMethods
+    {
+        public static IPEndPoint ToIPEndPoint(this NetworkAddress networkAddress)
+        {
+            var address = new IPAddress(networkAddress.IPv6Address.ToArray());
+            if (address.IsIPv4MappedToIPv6)
+                address = new IPAddress(networkAddress.IPv6Address.Skip(12).ToArray());
+
+            return new IPEndPoint(address, networkAddress.Port);
+        }
+    }
+}
