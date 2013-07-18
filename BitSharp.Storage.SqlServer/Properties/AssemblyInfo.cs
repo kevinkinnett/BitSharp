2a855bc37f3d414de7e7920f48bb5105d2f32bce
Paul <Paul@.(none)>
2013-07-18 18:16:16 -0300

    ensure storage provider assembly information is correct

--- BitSharp.Storage.SqlServer/Properties/AssemblyInfo.cs
+++ BitSharp.Storage.SqlServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BitSharp.Storage.Firebird")]
+[assembly: AssemblyTitle("BitSharp.Storage.SqlServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BitSharp.Storage.Firebird")]
+[assembly: AssemblyProduct("BitSharp.Storage.SqlServer")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- BitSharp.Storage/Properties/AssemblyInfo.cs
+++ BitSharp.Storage/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BitSharp.Database")]
+[assembly: AssemblyTitle("BitSharp.Storage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BitSharp.Database")]
+[assembly: AssemblyProduct("BitSharp.Storage")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
