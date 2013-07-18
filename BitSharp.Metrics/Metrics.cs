3ce0b0dba8acc09894c3c33d113024f8c8865b73
Paul <Paul@.(none)>
2013-07-18 18:11:22 -0300

    disable metrics

--- BitSharp.Metrics/Metrics.cs
+++ BitSharp.Metrics/Metrics.cs
@@ -16,80 +16,80 @@ namespace BitSharp.Metrics
     {
         public static void Main(string[] args)
         {
-            new MethodTimer().Time(() =>
-            {
-                TimeBlockEncode();
-                TimeBlockDecode();
+            //new MethodTimer().Time(() =>
+            //{
+            //    TimeBlockEncode();
+            //    TimeBlockDecode();
 
-                TimeTransactionEncode();
-                TimeTransactionDecode();
-            });
+            //    TimeTransactionEncode();
+            //    TimeTransactionDecode();
+            //});
         }
 
-        private static void TimeBlockEncode()
-        {
-            var blockProvider = new FileSystemBlockProvider();
-            var block = blockProvider.GetBlock(200.THOUSAND());
+    //    private static void TimeBlockEncode()
+    //    {
+    //        var blockProvider = new FileSystemBlockProvider();
+    //        var block = blockProvider.GetBlock(200.THOUSAND());
 
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
+    //        var stopwatch = new Stopwatch();
+    //        stopwatch.Start();
 
-            var count = 1 * 1000;
-            Parallel.For(0, count, i => block.With());
+    //        var count = 1 * 1000;
+    //        Parallel.For(0, count, i => block.With());
 
-            stopwatch.Stop();
-            var rate = ((float)count / ((float)stopwatch.ElapsedMilliseconds / 1000)).ToString("#,##0");
-            Debug.WriteLine("Block Encode:          {0,12} block/s".Format2(rate));
-        }
+    //        stopwatch.Stop();
+    //        var rate = ((float)count / ((float)stopwatch.ElapsedMilliseconds / 1000)).ToString("#,##0");
+    //        Debug.WriteLine("Block Encode:          {0,12} block/s".Format2(rate));
+    //    }
 
-        private static void TimeBlockDecode()
-        {
-            var blockProvider = new FileSystemBlockProvider();
-            var block = blockProvider.GetBlock(200.THOUSAND());
-            var blockBytes = block.ToRawBytes();
+    //    private static void TimeBlockDecode()
+    //    {
+    //        var blockProvider = new FileSystemBlockProvider();
+    //        var block = blockProvider.GetBlock(200.THOUSAND());
+    //        var blockBytes = block.ToRawBytes();
 
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
+    //        var stopwatch = new Stopwatch();
+    //        stopwatch.Start();
 
-            var count = 1.THOUSAND();
-            Parallel.For(0, count, i => Block.FromRawBytes(blockBytes));
+    //        var count = 1.THOUSAND();
+    //        Parallel.For(0, count, i => Block.FromRawBytes(blockBytes));
 
-            stopwatch.Stop();
-            var rate = ((float)count / ((float)stopwatch.ElapsedMilliseconds / 1000)).ToString("#,##0");
-            Debug.WriteLine("Block Decode:          {0,12} block/s".Format2(rate));
-        }
+    //        stopwatch.Stop();
+    //        var rate = ((float)count / ((float)stopwatch.ElapsedMilliseconds / 1000)).ToString("#,##0");
+    //        Debug.WriteLine("Block Decode:          {0,12} block/s".Format2(rate));
+    //    }
 
-        private static void TimeTransactionEncode()
-        {
-            var blockProvider = new FileSystemBlockProvider();
-            var block = blockProvider.GetBlock(200000);
+    //    private static void TimeTransactionEncode()
+    //    {
+    //        var blockProvider = new FileSystemBlockProvider();
+    //        var block = blockProvider.GetBlock(200000);
 
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
+    //        var stopwatch = new Stopwatch();
+    //        stopwatch.Start();
 
-            var count = 1.MILLION();
-            Parallel.For(0, count, i => block.Transactions.RandomOrDefault().With());
+    //        var count = 1.MILLION();
+    //        Parallel.For(0, count, i => block.Transactions.RandomOrDefault().With());
 
-            stopwatch.Stop();
-            var rate = ((float)count / ((float)stopwatch.ElapsedMilliseconds / 1000)).ToString("#,##0");
-            Debug.WriteLine("Transaction Encode:    {0,12} tx/s".Format2(rate));
-        }
+    //        stopwatch.Stop();
+    //        var rate = ((float)count / ((float)stopwatch.ElapsedMilliseconds / 1000)).ToString("#,##0");
+    //        Debug.WriteLine("Transaction Encode:    {0,12} tx/s".Format2(rate));
+    //    }
 
-        private static void TimeTransactionDecode()
-        {
-            var blockProvider = new FileSystemBlockProvider();
-            var block = blockProvider.GetBlock(200000);
-            var txBytesList = block.Transactions.Select(x => x.ToRawBytes()).ToList();
+    //    private static void TimeTransactionDecode()
+    //    {
+    //        var blockProvider = new FileSystemBlockProvider();
+    //        var block = blockProvider.GetBlock(200000);
+    //        var txBytesList = block.Transactions.Select(x => x.ToRawBytes()).ToList();
 
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
+    //        var stopwatch = new Stopwatch();
+    //        stopwatch.Start();
 
-            var count = 1.MILLION();
-            Parallel.For(0, count, i => Transaction.FromRawBytes(txBytesList.RandomOrDefault()));
+    //        var count = 1.MILLION();
+    //        Parallel.For(0, count, i => Transaction.FromRawBytes(txBytesList.RandomOrDefault()));
 
-            stopwatch.Stop();
-            var rate = ((float)count / ((float)stopwatch.ElapsedMilliseconds / 1000)).ToString("#,##0");
-            Debug.WriteLine("Transaction Decode:    {0,12} tx/s".Format2(rate));
-        }
+    //        stopwatch.Stop();
+    //        var rate = ((float)count / ((float)stopwatch.ElapsedMilliseconds / 1000)).ToString("#,##0");
+    //        Debug.WriteLine("Transaction Decode:    {0,12} tx/s".Format2(rate));
+    //    }
     }
 }
