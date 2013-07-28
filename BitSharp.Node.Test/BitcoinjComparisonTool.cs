84a9707265718b6a75a69ef970ecbc8fced05fce
Paul <Paul@.(none)>
2013-07-28 18:03:01 -0300

    add bitcoinj execution to comparison tool

--- BitSharp.Node.Test/BitcoinjComparisonTool.cs
+++ BitSharp.Node.Test/BitcoinjComparisonTool.cs
@@ -1,16 +1,13 @@
 ﻿using BitSharp.Blockchain;
+using BitSharp.Common.ExtensionMethods;
 using BitSharp.Daemon;
 using BitSharp.Network;
-using BitSharp.Node;
 using BitSharp.Script;
 using BitSharp.Storage;
 using BitSharp.Storage.Test;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace BitSharp.Node.Test
 {
@@ -38,7 +35,24 @@ namespace BitSharp.Node.Test
                     // start p2p client
                     localClient.Start();
 
-                    Thread.Sleep(5000);
+                    var projectFolder = Environment.CurrentDirectory;
+                    while (projectFolder.Contains(@"\bin"))
+                        projectFolder = Path.GetDirectoryName(projectFolder);
+
+                    File.Delete(Path.Combine(projectFolder, "Bitcoinj-comparison.log"));
+
+                    var javaProcessStartInfo = new ProcessStartInfo
+                        {
+                            FileName = @"C:\Program Files\Java\jdk1.7.0_25\bin\java.exe",
+                            WorkingDirectory = projectFolder,
+                            Arguments = @"-Djava.util.logging.config.file={0}\bitcoinj.log.properties -jar {0}\bitcoinj.jar".Format2(projectFolder),
+                            UseShellExecute = false
+                        };
+
+                    var javaProcess = Process.Start(javaProcessStartInfo);
+
+                    javaProcess.WaitForExit((int)TimeSpan.FromMinutes(5).TotalMilliseconds);
+                    Console.ReadLine();
                 }
             }
         }
