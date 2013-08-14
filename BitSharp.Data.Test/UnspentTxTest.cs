7a26d28cfaeb0cb5c5f6ec6d8fcfda1e0b69050d
Paul <Paul@.(none)>
2013-08-14 19:13:55 -0300

    create domain object to represent an unpsent transaction

---
+++ BitSharp.Data.Test/UnspentTxTest.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSharp.Data.Test
+{
+    [TestClass]
+    public class UnspentTxTest
+    {
+        [TestMethod]
+        public void TestUnspentTxIsDefault()
+        {
+            var defaultUnspentTx = default(UnspentTx);
+            Assert.IsTrue(defaultUnspentTx.IsDefault);
+
+            var randomUnspentTx = RandomData.RandomUnspentTx();
+            Assert.IsFalse(randomUnspentTx.IsDefault);
+        }
+
+        [TestMethod]
+        public void TestUnspentTxEquality()
+        {
+            var randomUnspentTx = RandomData.RandomUnspentTx();
+
+            var sameUnspentTx = new UnspentTx
+            (
+                txHash: randomUnspentTx.TxHash,
+                blockHash: randomUnspentTx.BlockHash,
+                txIndex: randomUnspentTx.TxIndex,
+                unspentOutputs: randomUnspentTx.UnspentOutputs
+            );
+
+            var differentUnspentTxBlockHash = new UnspentTx
+            (
+                txHash: randomUnspentTx.TxHash,
+                blockHash: ~randomUnspentTx.BlockHash,
+                txIndex: randomUnspentTx.TxIndex,
+                unspentOutputs: randomUnspentTx.UnspentOutputs
+            );
+
+            var differentUnspentTxTxIndex = new UnspentTx
+            (
+                txHash: randomUnspentTx.TxHash,
+                blockHash: randomUnspentTx.BlockHash,
+                txIndex: ~randomUnspentTx.TxIndex,
+                unspentOutputs: randomUnspentTx.UnspentOutputs
+            );
+
+            var differentUnspentTxTxHash = new UnspentTx
+            (
+                txHash: ~randomUnspentTx.TxHash,
+                blockHash: randomUnspentTx.BlockHash,
+                txIndex: randomUnspentTx.TxIndex,
+                unspentOutputs: randomUnspentTx.UnspentOutputs
+            );
+
+            var differentUnspentTxUnpsentOutputs = new UnspentTx
+            (
+                txHash: ~randomUnspentTx.TxHash,
+                blockHash: randomUnspentTx.BlockHash,
+                txIndex: randomUnspentTx.TxIndex,
+                unspentOutputs: randomUnspentTx.UnspentOutputs
+            );
+
+            Assert.IsTrue(randomUnspentTx.Equals(sameUnspentTx));
+            Assert.IsTrue(randomUnspentTx == sameUnspentTx);
+            Assert.IsFalse(randomUnspentTx != sameUnspentTx);
+
+            Assert.IsFalse(randomUnspentTx.Equals(differentUnspentTxBlockHash));
+            Assert.IsFalse(randomUnspentTx == differentUnspentTxBlockHash);
+            Assert.IsTrue(randomUnspentTx != differentUnspentTxBlockHash);
+
+            Assert.IsFalse(randomUnspentTx.Equals(differentUnspentTxTxIndex));
+            Assert.IsFalse(randomUnspentTx == differentUnspentTxTxIndex);
+            Assert.IsTrue(randomUnspentTx != differentUnspentTxTxIndex);
+
+            Assert.IsFalse(randomUnspentTx.Equals(differentUnspentTxTxHash));
+            Assert.IsFalse(randomUnspentTx == differentUnspentTxTxHash);
+            Assert.IsTrue(randomUnspentTx != differentUnspentTxTxHash);
+
+            Assert.IsFalse(randomUnspentTx.Equals(differentUnspentTxUnpsentOutputs));
+            Assert.IsFalse(randomUnspentTx == differentUnspentTxUnpsentOutputs);
+            Assert.IsTrue(randomUnspentTx != differentUnspentTxUnpsentOutputs);
+        }
+    }
+}
---
+++ BitSharp.Data/UnspentTx.cs
@@ -0,0 +1,69 @@
+﻿using BitSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSharp.Data
+{
+    public struct UnspentTx
+    {
+        private readonly UInt256 _blockHash;
+        private readonly UInt32 _txIndex;
+        private readonly UInt256 _txHash;
+        private readonly ImmutableBitArray _unspentOutputs;
+
+        private readonly bool notDefault;
+        private readonly int hashCode;
+
+        public UnspentTx(UInt256 blockHash, UInt32 txIndex, UInt256 txHash, ImmutableBitArray unspentOutputs)
+        {
+            this._blockHash = blockHash;
+            this._txIndex = txIndex;
+            this._txHash = txHash;
+            this._unspentOutputs = unspentOutputs;
+
+            this.notDefault = true;
+            this.hashCode = blockHash.GetHashCode() ^ txIndex.GetHashCode() ^ txHash.GetHashCode() ^ unspentOutputs.GetHashCode();
+        }
+
+        public bool IsDefault { get { return !this.notDefault; } }
+
+        public UInt256 BlockHash { get { return this._blockHash; } }
+
+        public UInt32 TxIndex { get { return this._txIndex; } }
+
+        public UInt256 TxHash { get { return this._txHash; } }
+
+        public ImmutableBitArray UnspentOutputs { get { return this._unspentOutputs; } }
+
+        public TxKey ToTxKey()
+        {
+            return new TxKey(this.BlockHash, this.TxIndex, this.TxHash);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is UnspentTx))
+                return false;
+
+            return (UnspentTx)obj == this;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.hashCode;
+        }
+
+        public static bool operator ==(UnspentTx left, UnspentTx right)
+        {
+            return left.BlockHash == right.BlockHash && left.TxIndex == right.TxIndex && left.TxHash == right.TxHash && left.UnspentOutputs == right.UnspentOutputs;
+        }
+
+        public static bool operator !=(UnspentTx left, UnspentTx right)
+        {
+            return !(left == right);
+        }
+    }
+}
