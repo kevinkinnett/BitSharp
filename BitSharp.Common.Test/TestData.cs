0711665a63b76ce445d74aa51f6ea882dcdcae71
vishnu0110 <72989198+vishnu0110@users.noreply.github.com>
2020-10-16 21:32:29 +0530

    Hack check

--- BitSharp.Common.Test/TestData.cs
+++ BitSharp.Common.Test/TestData.cs
@@ -8,7 +8,7 @@ namespace BitSharp.Common.Test
 {
     public static class TestData
     {
-        public static readonly string HEX_STRING_32 = "4a5e1e4baab89f3a32518a88c31bc87f";
-        public static readonly string HEX_STRING_64 = "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b";
+        सार्वजनिक  स्थैतिक  केवल पढ़ने के लिए  स्ट्रिंग  HEX_STRING_32  =  "4a5e1e4baab89f3a32518a88c31bc87f" ;
+        सार्वजनिक    स्थैतिक    केवल   पढ़ने   के   लिए    स्ट्रिंग    HEX_STRING_64    =    "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7af; loveyao
     }
 }
